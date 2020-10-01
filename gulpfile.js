d96b8c7e17f4b0300d05a1a04ed4548785e0d0a5
Aleksey Rogach <alekss.r@mail.ru>
2020-10-01 16:28:33 +0400

    Добавляет настройки для плагина, оптимизирующего изображения

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,8 @@ let { src, dest } = require('gulp'),
    group_media = require("gulp-group-css-media-queries"),   
    clean_css = require("gulp-clean-css"),
    rename = require("gulp-rename"),
-   uglify = require("gulp-uglify-es").default;
+   uglify = require("gulp-uglify-es").default,
+   imagemin = require("gulp-imagemin");
  
 function browserSync(params) {
     browsersync.init({
@@ -100,7 +101,15 @@ function js() {
 }
 
 function images() {
-    return src(path.src.img)       
+    return src(path.src.img)    
+       .pipe(
+           imagemin({
+               progressive: true,
+               svgoPlugins: [{removeViewBox: false}],
+               interlaced: true,
+               optimizationLevel: 3 // 0 to 7
+           })
+       )
        .pipe(dest(path.build.img))
        .pipe(browsersync.stream())
 }
@@ -108,16 +117,18 @@ function images() {
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js);    
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);     
 }
  
 function clean(params) {
     return del(path.clean);
 }
  
-let build = gulp.series(clean, gulp.parallel(js, css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles,browserSync);
  
+exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
